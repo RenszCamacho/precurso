b547b8582b9861ea20139e00165f303ff10b2dd3
Renszo Camacho <varsovia84@gmail.com>
2021-02-24 15:48:53 +0100

    Is it an Integer Function.

--- calculator-pro/calculator-pro.js
+++ calculator-pro/calculator-pro.js
@@ -1,3 +1,11 @@
+const isInteger = (resultNumber) => {
+  if (Number.isInteger(resultNumber)) {
+    return resultNumber;
+  } else {
+    resultNumber.toFixed(3);
+  }
+};
+
 const calculator = (arr) => {
   const sum = arr.reduce((acc, num) => acc + num, 0),
     substraction = arr.reduce((acc, num) => acc - num),
@@ -10,7 +18,7 @@ const calculator = (arr) => {
     return `The squareRoot of ${arr[0]} is ${squareRoot}`;
   } else {
     return [
-      `Result of sum is ${sum}`,
+      `Result of sum is ${isInteger(sum)}`,
       `Result of substraction is ${substraction}`,
       `Result of multiplication is ${multiplication}`,
       `Result of division is ${division}`,
