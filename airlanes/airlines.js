1ee3c47e0d339c639ca670bb55e8b6b182afb1e3
Renszo Camacho <varsovia84@gmail.com>
2021-02-26 14:42:14 +0100

    Fixing some bugs.

--- airlanes/airlines.js
+++ airlanes/airlines.js
@@ -1,24 +1,14 @@
 let flights = [
   { id: 00, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
-
   { id: 01, to: "New York", from: "Barcelona", cost: 700, scale: false },
-
   { id: 02, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
-
   { id: 03, to: "Paris", from: "Barcelona", cost: 210, scale: false },
-
   { id: 04, to: "Roma", from: "Barcelona", cost: 150, scale: false },
-
   { id: 05, to: "London", from: "Madrid", cost: 200, scale: false },
-
   { id: 06, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
-
   { id: 07, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
-
   { id: 08, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
-
   { id: 09, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
-
   { id: 10, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
 ];
 
@@ -28,19 +18,21 @@ const greeting = () => {
   let userName = prompt("Hello, could you please introduce your name.");
   alert(`Hello ${userName}, welcome! You can see the results on the console.`);
   return console.log(
-    `Hello ${userName}, welcome! You can see the results on the console.`
+    `Hello ${userName}, welcome! You can see all the flights from today.`
   );
 };
 
 const displayingFlights = (arrFlights) => {
-  info("List of all flight today");
+  info("List of all the flight today");
 
   const display = arrFlights.map((flight) =>
     flight.scale
-      ? `- The flight with origin: ${flight.from}, and destination: ${flight.to} has a cost of ${flight.cost}€ and makes a stop.
+      ? `
+      - The flight with origin: ${flight.from}, and destination: ${flight.to} has a cost of ${flight.cost}€ and makes a stop.
 
     `
-      : `- The flight with origin: ${flight.from}, and destination: ${flight.to} has a cost of ${flight.cost}€ and does not make any stop.
+      : `
+      - The flight with origin: ${flight.from}, and destination: ${flight.to} has a cost of ${flight.cost}€ and does not make any stop.
 
     `
   );
@@ -59,11 +51,13 @@ const averagePrices = () => {
 const flightsWithStop = (flights) => {
   info("List of flights with stop.");
 
-  const filterFlightsScale = flights.filter((flight) => flight.scale);
-  const printFlights = filterFlightsScale.map(
+  const filterFlightsStop = flights.filter((flight) => flight.scale);
+  const printFlights = filterFlightsStop.map(
     (flight) =>
       `
-      - The flight with origin ${flight.from}, and destination: ${flight.to}, has a cost of ${flight.cost}€ and this flight makes a stop.`
+      - The flight with origin ${flight.from}, and destination: ${flight.to}, has a cost of ${flight.cost}€ and this flight makes a stop.
+
+    `
   );
   return console.log(...printFlights);
 };
@@ -78,7 +72,9 @@ const lastFlights = (flights) => {
   const printLastFligths = filterLastFlights.map(
     (flight) =>
       `
-      - The flight with origin ${flight.from}, and destination: ${flight.to}, has a cost of ${flight.cost}€ and this flight makes a stop.`
+      - The flight with origin ${flight.from}, and destination: ${flight.to}, has a cost of ${flight.cost}€ and this flight makes a stop.
+
+    `
   );
 
   return console.log(...printLastFligths);
